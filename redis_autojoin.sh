542672f63f8ffb4f4ebf1d389e768bcfeaf68d3a
Steven Fluck <4992472+7-plus-t@users.noreply.github.com>
2019-09-18 17:52:27 +0200

    #15 Improve redis_autojoin.sh
    Handle Hipchat versions where the Redis config is stored in the Hipchat site config file.
    Thanks @anx-ag for the patch.

--- redis_autojoin.sh
+++ redis_autojoin.sh
@@ -3,20 +3,35 @@ set -e
 
 # Get autojoin configuration for Hipchat users from Redis. Script is best run on a Hipchat App node.
 
-export PGHOST=$(cat /hipchat/config/site.json | jq -r '.databases.hipchat_postgres.servers[0]' | cut -d: -f1)
-export PGUSER=$(cat /hipchat/config/site.json | jq -r '.databases.hipchat_postgres.user')
-export PGSCHEMA=$(cat /hipchat/config/site.json | jq -r '.databases.hipchat_postgres.schema')
-export PGPASSWORD=$(cat /hipchat/config/site.json | jq -r '.databases.hipchat_postgres.pass')
-
-RHOST=$(psql -h $PGHOST -U $PGUSER -d $PGSCHEMA -t -c "SELECT value FROM configurations WHERE key='redishostname';")
-RPORT=$(psql -h $PGHOST -U $PGUSER -d $PGSCHEMA -t -c "SELECT value FROM configurations WHERE key='redisport';")
-RPWD=$(psql -h $PGHOST -U $PGUSER -d $PGSCHEMA -t -c "SELECT value FROM configurations WHERE key='redispass';")
-
+SITECONFIG=/hipchat/config/site.json
 OUTPUT_FILE=./autojoin.json
+
+# check if the redis configuration is stored directly in the site config
+export RHOST=$(jq -r .redis[0].host $SITECONFIG)
+if [ "$RHOST" != "null" ]; then
+  export RPORT=$(jq -r .redis[0].port $SITECONFIG)
+  export RPWD=$(jq -r .redis[0].auth $SITECONFIG)
+else
+  export PGHOST=$(jq -r '.databases.hipchat_postgres.servers[0]' $SITECONFIG | cut -d: -f1)
+  export PGUSER=$(jq -r '.databases.hipchat_postgres.user' $SITECONFIG)
+  export PGSCHEMA=$(jq -r '.databases.hipchat_postgres.schema' $SITECONFIG)
+  export PGPASSWORD=$(jq -r '.databases.hipchat_postgres.pass' $SITECONFIG)
+
+  export RHOST=$(psql -h $PGHOST -U $PGUSER -d $PGSCHEMA -t -c "SELECT value FROM configurations WHERE key='redishostname';")
+  export RPORT=$(psql -h $PGHOST -U $PGUSER -d $PGSCHEMA -t -c "SELECT value FROM configurations WHERE key='redisport';")
+  export RPWD=$(psql -h $PGHOST -U $PGUSER -d $PGSCHEMA -t -c "SELECT value FROM configurations WHERE key='redispass';")
+fi
+
+if [ "$RPWD" != "null" ]; then
+  RPWD="-a $RPWD"
+else
+  RPWD=""
+fi
+
 echo "{\"autojoins\": [" > $OUTPUT_FILE
-for key in $(redis-cli -h $RHOST -p $RPORT -a $RPWD KEYS '*' | grep  "pref:autoJoin:" | cut -d" " -f2); do
+for key in $(redis-cli -h $RHOST -p $RPORT $RPWD KEYS '*' | grep  "pref:autoJoin:" | cut -d" " -f2); do
   echo "Found key: $key"
-  VALUE=$(redis-cli -h $RHOST -p $RPORT -a $RPWD GET "$key")
+  VALUE=$(redis-cli -h $RHOST -p $RPORT $RPWD GET "$key")
   [[ ${#VALUE} -eq 4 && "$VALUE" -eq "None" ]] && VALUE="[]"
   USER_ID=$(echo $key | cut -d":" -f3)
   echo "{\"user_id\": $USER_ID, \"rooms\": $VALUE }," >> $OUTPUT_FILE
